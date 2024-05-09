634f02b788304db9ab264a76b8f5d85e2651068c
Tensor <narutokelechi@gmail.com>
2024-05-09 15:18:21 +0100

    Vision tranformer code

--- ViT/vision_transformer.py
+++ ViT/vision_transformer.py
@@ -1,5 +1,6 @@
 import torch 
 import torch.nn as nn
+from einops import rearrange, repeat
 from transformers import SiglipVisionModel, SiglipTextModel, SiglipModel
 
 class FeedForwardLayer(nn.Module):
@@ -47,6 +48,7 @@ class Attention(nn.Module):
         
         return x
         
+        
 class ImagePatching(nn.Module):
     def __init__(self, in_channels=3, embed_dim=768, patch_size=16):
         super().__init__()
@@ -57,3 +59,78 @@ class ImagePatching(nn.Module):
         return image_patches
     
 
+class Encoder_block(nn.Module):
+    def __init__(self, model_dim, n_heads=8, ff_ratio=4, drop_rate=0.0):
+        super().__init__()
+        self.ln1 = nn.LayerNorm(model_dim)
+        self.attention = Attention(model_dim, n_heads, drop_rate, drop_rate)
+        self.ln2 = nn.LayerNorm(model_dim)
+        self.feed_forward = FeedForwardLayer(model_dim, model_dim * ff_ratio, model_dim, dropout=drop_rate)
+        
+    def forward(self, x):
+        x1 = self.ln1(x)
+        x2 = x + self.attention(x1)
+        x = x + self.ln2(self.feed_forward(x2))
+        
+        return x
+    
+class Transformer(nn.Module):
+    def __init__(self, depth, model_dim, n_heads=8, ff_ratio=4, drop_rate=0.0):
+        super().__init__()
+        self.encoder_blocks = nn.ModuleList(
+            [Encoder_block(model_dim, n_heads, ff_ratio, drop_rate) for _ in range(depth)]
+        )
+        
+    def forward(self, x):
+        for enc_block in self.encoder_blocks:
+            x = enc_block(x)
+        
+        return x
+    
+    
+class VisionTransformer(nn.Module):
+    def __init__(self, img_size=224, patch_size=16, in_channels=3, num_classes=100, embed_dim=768, depth=12, n_heads=12, ff_ratio=4, drop_rate=0.0):
+        super().__init__()
+        if img_size % patch_size != 0:
+            raise ValueError('Image size must be divisible by patch size.')
+        
+        num_patches = (img_size // patch_size) ** 2
+        self.patch_size = patch_size
+        
+        self.image_patches = ImagePatching(in_channels, embed_dim, self.patch_size)
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, embed_dim))
+        self.class_embed = nn.Parameter(torch.zeros(1, 1, embed_dim))
+        
+        nn.init.trunc_normal_(self.pos_embed, std=0.2)
+        nn.init.trunc_normal_(self.class_embed, std=0.2)
+        
+        self.drop = nn.Dropout(p=drop_rate)
+        self.transformer = Transformer(depth, embed_dim, n_heads, ff_ratio, drop_rate)
+        self.normalize = nn.LayerNorm(embed_dim)
+        self.output_layer = nn.Linear(embed_dim, num_classes)
+        
+        self.apply(self._init_weights)
+        
+    def _init_weights(self, v):
+        if isinstance(v, nn.Linear):
+            nn.init.trunc_normal_(v.weight, std=0.2)
+            if isinstance(v, nn.Linear) and v.bias is not None:
+                nn.init.constant_(v.bias, 0)
+                
+        elif isinstance(v, nn.LayerNorm):
+            nn.init.constant_(v.bias, 0)
+            nn.init.constant_(v.weight, 1.0)
+    
+    def forward(self, x):
+        b = x.shape[0]
+        class_token = self.class_embed.expand(b, -1, -1)
+        
+        x = self.image_patches(x)
+        x = torch.cat((class_token, x), dim=1)
+        x += self.pos_embed
+        x = self.transformer(self.drop(x))
+        x = self.normalize(x)
+        
+        x_out = self.output_layer(x[:, 0])
+        
+        return x_out
