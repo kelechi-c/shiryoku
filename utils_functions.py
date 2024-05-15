c3d8ffbeac8c297e23d9d5293bc9f103eaed1be8
Tensor <narutokelechi@gmail.com>
2024-05-15 13:09:45 +0100

    Added text tokenization and image transformation

--- utils_functions.py
+++ utils_functions.py
@@ -1,12 +1,30 @@
 import cv2
+import nltk
 from transformers import AutoTokenizer
 from datasets import load_dataset, Image
 import requests
 import PIL.Image as pillow_image
 from nltk import tokenize
+from torchvision import transforms
+import re
+from collections import Counter
+from config import Config
 
-tokenizer = tokenize.
 # tokenizer = AutoTokenizer.from_pretrained("google/siglip-base-patch16-224")
+
+# For image data
+def image_transforms(image_file):
+    trasnformed_image = transforms.Compose(
+        [
+            transforms.RandomCrop(Config.image_size),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        ]
+    )
+
+    return trasnformed_image
+
 
 def read_img(image_file):
     image = cv2.imread(image_file)
@@ -16,18 +34,52 @@ def read_img(image_file):
     return image
 
 def get_image_from_url(url):
-    url_content = requests.get(url, stream=True).raw
-    image = pillow_image.open(url_content)
-    
-    return image
+    try:
+        url_content = requests.get(url, stream=True).raw
+        image = pillow_image.open(url_content)
+        return image
+    except Exception as e:
+        print(e)
+        
     
 
+
+# For text data
 def tokenize_text(text_input):
-    text_tokens = tokenizer.encode(text_input, truncation=True, max_length=64, padding="max_length", return_tensors="pt")
-    
+    text_tokens = tokenize.word_tokenize(str(text_input).lower())
+
     return text_tokens
 
 
+def preprocess_text(text):
+    text = re.sub(r"[^a-z0-9\s]", "", text)  
+    tokens = tokenize_text(text)  
+    tokens = [
+        t for t in tokens if t not in nltk.corpus.stopwords.words("english")
+    ] 
+    return tokens
+
+
+def create_vocabulary(text_dataset):
+    all_tokens = []
+    for text in text_dataset:
+        tokens = preprocess_text(text)
+        all_tokens.extend(tokens)
+
+    vocab_counter = Counter(all_tokens)
+    vocab = [
+        word for word, count in vocab_counter.most_common() if count >= 1
+    ]
+    vocab = ["<pad>", "<start>", "<end>", "<unk>"] + vocab
+
+    word_to_idx = {word: idx for idx, word in enumerate(vocab)}
+    idx_to_word = {idx: word for idx, word in enumerate(vocab)}
+
+    return word_to_idx, idx_to_word
+
+
+
+# for fetching the required datasets
 def get_dataset(data_split):
     docci_dataset = load_dataset("google/docci")
 
@@ -40,6 +92,7 @@ def get_dataset(data_split):
     img_captions = [caption for caption in descriptions]
     
     return images, img_captions
+
 
 def get_moondream_dataset():
     moondream_dataset = load_dataset("isidentical/moondream2-coyo-5M-captions")
