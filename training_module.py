f95a04d3c2f55e6b6fce9a935fe73ada38ca8d98
Tensor <narutokelechi@gmail.com>
2024-05-20 13:37:21 +0100

    Added modififcations to training module, validation

--- training_module.py
+++ training_module.py
@@ -23,7 +23,46 @@ epochs = Config.num_epochs
 def training_loop(train_loader, lossfn, optimizer, epochs=epochs):
     for epoch in tqdm(range(epochs)):
         print(f'Training epoch {epoch}')
-        for _, (images, captions, lengths) in enumerate(train_loader):
+        for _, (images, captions, lengths) in tqdm(enumerate(train_loader)):
+            images = images.to(device)
+            captions = captions.to(device)
+            targets = pack_padded_sequence(captions, lengths, batch_first=True)[0]
+
+            features = encoder(images)
+            outputs = decoder(features, captions, lengths)
+            loss = lossfn(outputs, targets)
+
+            decoder.zero_grad()
+            encoder.zero_grad()
+            loss.backwards()
+            optimizer.step()
+            
+            val_loss = 0.0
+            
+            encoder.eval()
+            decoder.eval()  
+            # Set the model to evaluation mode
+        with torch.no_grad():  # Disable gradient computation
+            for inputs, labels in valid_loader:
+                features = encoder(images)
+                outputs = decoder(features, captions, lengths)
+                loss = lossfn(outputs, targets)
+                val_loss += loss.item()
+
+
+            print(f'Epoch {epoch} of {epochs}, loss: {loss.item():.4f}')
+
+        print(f"End metrics for {epoch} of {epochs}, loss: {loss.item():.4f}")
+
+        torch.save(decoder.state_dict(), os.path.join(Config.model_output_path, f'decoder_{epoch}.pth'))
+        torch.save(encoder.state_dict(), f"encoder_{epoch}.pth")
+
+        print(f"Epoch {epoch} complete")
+
+def validation_loop(valid_loader, lossfn):
+    for epoch in tqdm(range(epochs)):
+        print(f'Validation epoch {epoch}')
+        for _, (images, captions, lengths) in tqdm(enumerate(valid_loader)):
             images = images.to(device)
             captions = captions.to(device)
             targets = pack_padded_sequence(captions, lengths, batch_first=True)[0]
@@ -37,11 +76,9 @@ def training_loop(train_loader, lossfn, optimizer, epochs=epochs):
             loss.backwards()
             optimizer.step()
 
-            print(f'Epoch {epoch} of {epochs}, loss: {loss.item():.4f}')
+            print(f'Validating {epoch} of {epochs}, loss: {loss.item():.4f}')
 
         print(f"End metrics for {epoch} of {epochs}, loss: {loss.item():.4f}")
+        
 
-        torch.save(decoder.state_dict(), os.path.join(Config.model_output_path, f'decoder_{epoch}.pth'))
-        torch.save(encoder.state_dict(), f"encoder_{epoch}.pth")
-
-        print(f"Epoch {epoch} complete")
+training_loop(train_loader, criterion, optimizer)
