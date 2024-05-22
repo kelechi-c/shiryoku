1e9585fff579cc246cb9638fa7cd92746b3c08c9
Tensor <narutokelechi@gmail.com>
2024-05-22 12:30:48 +0100

    Merged encoder and decoder into one layer

--- shiryoku_v1/shiryoku_model.py
+++ shiryoku_v1/shiryoku_model.py
@@ -1,27 +1,24 @@
 import torch
 import torch.nn as nn  
 from einops import rearrange                                                  
-
 from image_encoder import ConvNetEncoder
 from shiryoku_v1.text_model import TextRNNDecoder
-from mixtral_of_experts import MixtralOfExpertsLayer
-from ViT.vision_transformer import VisionTransformer
 from config import Config
 from utils_functions import *
 
 
-# class ModalityFusion(nn.Module):
-#     def __init__(self):
-#         super().__init__()
-#         self.vision_encoder = ConvNetEncoder()
-#         self.text_embedding = TextRNNDecoder()
+class ImageTextModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.vision_encoder = ConvNetEncoder()
+        self.text_decoder = TextRNNDecoder(
+            vocab_size=Config.vocab_size,
+            embed_dim=Config.embed_size,
+            hidden_size=Config.hidden_size,
+        )
 
-#     def forward(self, x):
-#         visual_tokens = self.vision_encoder(x)
-#         text_tokens = self.text_embedding(x)
-        
-#         token_fusion = torch.cat((visual_tokens, text_tokens), dim=1) 
-        
-#         return token_fusion
-    
+    def forward(self, image, text, lengths):
+        visual_tokens = self.vision_encoder(image)
+        decoder_output = self.text_decoder(visual_tokens, text, lengths)
 
+        return decoder_output
