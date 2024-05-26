2fdca1d21e78c456e9451666359c59c9ca47610e
Tensor <narutokelechi@gmail.com>
2024-05-26 19:34:09 +0100

    Image download and csv creation

--- moondream_dl.py
+++ moondream_dl.py
@@ -1,3 +1,4 @@
+from typing import List
 import urllib
 import requests
 import os
@@ -48,40 +49,43 @@ def download_img(url):
             yield image_file
 
             print(f"{out_file} downloaded")
-            
+
     except Exception as e:
         print(f"error: {e}")
 
 
-def load_image_wrapper(url):
-    try:
-        image = download_img(url)
-        return image
-    except:
-        return None
-
-
 def get_moondream_data():
     moondream_dataset = load_dataset("isidentical/moondream2-coyo-5M-captions")
-    md_data = moondream_dataset["train"][:10]  # type: ignore
+    md_data = moondream_dataset["train"][:100000]  # type: ignore
     image_urls = md_data["url"]  # type: ignore
     descriptions = md_data["moondream2_caption"]  # type: ignore
 
-    with Pool(processes=7) as pool:
-        images = pool.imap(load_image_wrapper, tqdm(image_urls))
-        captions = [desc.lower() for desc in descriptions]
-        
+    count = 0
 
-    for image, caption in tqdm(zip(images, captions)):
-        if image is not None:
-            yield (image_urls, image, caption)
-            
-    
-        
+    for url, desc in tqdm(zip(image_urls, descriptions)):
+        image_dl = download_img(url)
+        caption = desc.lower()
 
-def moondream_csv(dataframe: pd.DataFrame, output_path: str):
-    print('Writing to csv..')
-    dataframe.to_csv(output_path)
-    print('Csv transfer complete...')
-    
-    
+        count += 1
+
+        if image_dl is not None:
+            print(f"image no.{count}")
+            yield (image_dl, image_dl.name, caption) # type: ignore
+
+
+q, k, v = zip(*get_moondream_data())
+
+
+def moondream_csv(path: List, url: List, desc: List, output_path: str = os.getcwd()):
+    print("Writing to csv..")
+
+    keys = ["image_path", "image_url", "caption"]
+    md_dict = dict(zip(keys, zip(path, url, desc)))
+    moondream_df = pd.DataFrame(md_dict)
+
+    moondream_df.to_csv("moondream2.csv")
+
+    print("Csv transfer complete...")
+
+
+moondream_csv(q, k, v)
